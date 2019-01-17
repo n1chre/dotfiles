c8ff0434e9d333555d260eb809872c99dec7a219
Filip Hrenić <hrenic.filip@gmail.com>
2019-01-17 23:18:47 +0000

    add noclobber

--- shell/options.sh
+++ shell/options.sh
@@ -1,6 +1,12 @@
 # default umask
 umask 022
+
 # fail if any command in pipe fails, not only last one
 set -o pipefail
+
 # vi key bindings
 set -o vi
+
+# don't allow `$cmd > existing_file`
+# overwrite with `$cmd >! existing_file`
+set -o noclobber
