ebb0f03b073eb0e4bc462925c9d1b5d73f8d202a
Filip Hrenić <hrenic.filip@gmail.com>
2019-02-07 03:46:07 -0800

    Revert "Revert "fix fzf""
    This reverts commit 6b9df1c04770dd6b0051d9c5a387d6aecf9ec776.

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -6,13 +6,19 @@ if [[ "${current_shell}" = *bash ]]; then
 elif [[ "${current_shell}" = *zsh ]]; then
   FZF_FILE=~/.fzf.zsh
 else
-  FZF_FILE=
+  echo "Unknown shell: ${current_shell}"
+  return
+fi
+unset current_shell
+
+if [ ! -f ${FZF_FILE} ]; then
+  "${FZF_FILE} doesn't exist, reinstall fzf?"
+  unset FZF_FILE
+  return
 fi
 
-[ ! -f ${FZF_FILE} ] && unset FZF_FILE current_shell ; return
-
 source $FZF_FILE
-unset FZF_FILE current_shell
+unset FZF_FILE
 
 export FZF_DEFAULT_COMMAND='ag -U --hidden --ignore .git -g ""'
 export FZF_DEFAULT_OPTS='--bind "F1:toggle-preview" --preview "rougify {} 2> /dev/null || cat {} 2> /dev/null || tree -C {} 2> /dev/null | head -100" --color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229
