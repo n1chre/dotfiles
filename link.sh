822ce3f7c0f27b1ce0c972069c3f7f74512ab4a3
Filip Hrenić <hrenic@hrenic-mbp.dhcp.thefacebook.com>
2019-03-14 11:05:56 -0700

    move stuff around

--- link.sh
+++ link.sh
@@ -2,10 +2,10 @@
 DOTFILES="${HOME}/dotfiles"
 
 ln -fs "${DOTFILES}/bash/bashrc" ~/.bashrc
-ln -fs "${DOTFILES}/gitconfig" ~/.gitconfig
-ln -fs "${DOTFILES}/gitignore" ~/.gitignore
-ln -fs "${DOTFILES}/inputrc" ~/.inputrc
-ln -fs "${DOTFILES}/pythonrc" ~/.pythonrc
-ln -fs "${DOTFILES}/tmux.conf" ~/.tmux.conf
+ln -fs "${DOTFILES}/configs/gitconfig" ~/.gitconfig
+ln -fs "${DOTFILES}/configs/gitignore" ~/.gitignore
+ln -fs "${DOTFILES}/configs/inputrc" ~/.inputrc
+ln -fs "${DOTFILES}/configs/pythonrc" ~/.pythonrc
+ln -fs "${DOTFILES}/configs/tmux.conf" ~/.tmux.conf
 ln -fs "${DOTFILES}/zsh/zshrc" ~/.zshrc
 ln -fs "${DOTFILES}/zsh/zshenv" ~/.zshenv
