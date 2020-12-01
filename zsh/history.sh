c3f38095e7c15e18b0017b5fcc0f4822b9d16a99
Filip Hrenić <hrenic.filip@gmail.com>
2020-12-01 04:45:29 -0800

    Dont share history between panes/windows

--- zsh/history.sh
+++ zsh/history.sh
@@ -15,7 +15,7 @@ setopt extended_history
 # Write to the history file immediately, not when the shell exits.
 setopt inc_append_history
 # Share history between all sessions.
-setopt share_history
+# setopt share_history
 # Expire a duplicate event first when trimming history.
 setopt hist_expire_dups_first
 # Do not record an event that was just recorded again.
